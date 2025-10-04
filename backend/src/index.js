93518f19eaa5b5688eefe2604bf64619da69c73a
Kadir Zeybekoğlu <kadrzeybek@gmail.com>
2025-10-04 16:57:33 +0300

    backend part completely done an ıt is ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import cors from 'cors'
+import path from 'path'
 
 import productRoutes from './routes/product.route.js'
 
@@ -8,8 +10,24 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}
+))
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    
+    app.get(/.*/, (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    });
+  }
 
 app.listen(PORT, () =>{
     console.log("App is running on port: ", PORT)
